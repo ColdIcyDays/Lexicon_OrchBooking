af0b84fc10c46b3f4468027ecb3ab5265528d7de
Erik Lj <erik.ljungman@gmail.com>
2026-06-23 15:27:24 +0200

    Removed ID from OrchUploadVenueRequest
     - Title.

--- Lexicon_OrchBookingBackend/Controllers/DTOs/OrchUploadVenueRequest.cs
+++ Lexicon_OrchBookingBackend/Controllers/DTOs/OrchUploadVenueRequest.cs
@@ -3,8 +3,7 @@
 namespace Lexicon_OrchBookingBackend.Controllers.DTOs;
 
 public class OrchUploadVenueRequest
-{
-    public int Id { get; set; }
+{ 
     public string Name { get; set; }
     public string Address { get; set; }
     public int MaxSeating { get; set; }
