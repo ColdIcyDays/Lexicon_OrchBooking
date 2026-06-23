eb040156b1e5dde758e8f884f0b652799e2651a0
Erik Lj <erik.ljungman@gmail.com>
2026-06-23 15:26:55 +0200

    Added OrchGetVenuesResult
     - Title.

---
+++ Lexicon_OrchBookingBackend/Controllers/DTOs/OrchGetVenuesResult.cs
@@ -0,0 +1,8 @@
+﻿using Lexicon_OrchBookingBackend.Models;
+
+namespace Lexicon_OrchBookingBackend.Controllers.DTOs;
+
+public class OrchGetVenuesResult
+{
+    public List<OrchVenue> Venues { get; set; }
+}
