cff4be6fb42975ff21389621d7aadf5d8d88c413
Erik Lj <erik.ljungman@gmail.com>
2026-06-23 15:27:09 +0200

    Added UserData to OrchInfoData
     - Title.

--- Lexicon_OrchBookingBackend/Controllers/DTOs/OrchInfoData.cs
+++ Lexicon_OrchBookingBackend/Controllers/DTOs/OrchInfoData.cs
@@ -1,8 +1,12 @@
-﻿namespace Lexicon_OrchBookingBackend.Controllers.DTOs;
+﻿using Lexicon_OrchBookingBackend.Models;
+
+namespace Lexicon_OrchBookingBackend.Controllers.DTOs;
 
 public class OrchInfoData
 {
     public string Username { get; set; }
     public string Email { get; set; }
     public string[] Roles { get; set; }
+
+    public UserData Data { get; set; }
 }
